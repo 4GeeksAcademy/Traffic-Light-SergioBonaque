eb9e8a55a8c3f6bc46652e8b75ba18e565360115
Sergio Bonaque <s.bonaquecontacto@gmail.com>
2025-11-08 20:02:25 +0000

    center line

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -7,19 +7,24 @@ export function Home() {
 	const [selectedColor, setSelectedColor] = useState("yellow");
 
 	return (
-		<div className="traffic-light">
-			<div
-				onClick={() => setSelectedColor("red")}
-				className={"light red" + (selectedColor === "red" ? " glow" : "")}
-			></div>
-			<div
-				onClick={() => setSelectedColor("yellow")}
-				className={"light yellow" + (selectedColor === "yellow" ? " glow" : "")}
-			></div>
-			<div
-				onClick={() => setSelectedColor("green")}
-				className={"light green" + (selectedColor === "green" ? " glow" : "")}
-			></div>
+		<div className="center-wrapper">
+			<div className="d-flex stick">jhu
+
+			</div>
+			<div className="traffic-light ">
+				<div
+					onClick={() => setSelectedColor("red")}
+					className={"light red" + (selectedColor === "red" ? " glow" : "")}
+				></div>
+				<div
+					onClick={() => setSelectedColor("yellow")}
+					className={"light yellow" + (selectedColor === "yellow" ? " glow" : "")}
+				></div>
+				<div
+					onClick={() => setSelectedColor("green")}
+					className={"light green" + (selectedColor === "green" ? " glow" : "")}
+				></div>
+			</div>
 		</div>
 	);
 }
